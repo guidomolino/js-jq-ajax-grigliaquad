e56ab6259e2ea6317219cfbb5f3062eb04e57173
Guido <molido@hotmail.it>
2020-07-21 16:12:51 +0200

    Terzo Commit

--- js/script.js
+++ js/script.js
@@ -9,11 +9,36 @@
 
 function clickQuadrato() {
   var quadrato = $(".quadrato");
-  quadrato.addEventListener("click", insNum);
+  quadrato.click(insNum);
 }
 
 function insNum() {
-  https://flynn.boolean.careers/exercises/api/random/int
+
+  $.ajax({
+
+    url: "https://flynn.boolean.careers/exercises/api/random/int",
+    method: "Get",
+    success: function(data, state){
+      var success = data["success"];
+      var value = data["response"];
+
+      if (success) {
+        console.log(value);
+        $(".quadrato p").text(value);
+
+        if (value<=5) {
+          $(".quadrato").addClass("minore");
+        }else {
+          $(".quadrato").addClass("maggiore");
+        }
+      }
+    },
+    error: function(request, state, error) {
+      console.log(state);
+      console.log(request);
+      console.log(error);
+    }
+  });
 }
 
 
